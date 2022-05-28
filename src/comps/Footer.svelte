16d8036efd3f815b0d8a7031173ab67cb663e7ab
Teg <105562627+TegFox@users.noreply.github.com>
2022-05-27 19:46:28 -0700

    Fixed the issue with the footer

--- src/comps/Footer.svelte
+++ src/comps/Footer.svelte
@@ -7,11 +7,13 @@
     <div class="fh1">
         You've Reached The End.
     </div>
-    <a on:click={() => scrollTop()} href="" style:text-decoration="none" style:color="grey" style:pointer="pointer">
-        <div style:margin="0.5rem 0">
-            Go back to top
-        </div>
-    </a>
+    <div style="display: flex; flex-direction: row">
+        <a on:click={() => scrollTop()} href="" style:text-decoration="none" style:color="grey" style:pointer="pointer">
+            <div style:margin="0.5rem 0">
+                Go back to top
+            </div>
+        </a>
+    </div>
     <p>
         © Teg Fox {new Date().getFullYear()}. All Rights Reserved.
     </p>
