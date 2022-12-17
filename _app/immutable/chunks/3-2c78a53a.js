8138d238aeea5fc8d9c016712a5b5aef18575fd1
Teg <105562627+TegFox@users.noreply.github.com>
2022-12-16 20:59:51 -0500

    Updates

--- _app/immutable/chunks/3-2c78a53a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/404/_page.svelte-a05c43e8.js";export{t as component};
--- _app/immutable/components/pages/404/_page.svelte-43655eaa.js
+++ _app/immutable/components/pages/404/_page.svelte-43655eaa.js
@@ -2,4 +2,4 @@ import{S as i,i as c,s as u,a as p,w as l,W as f,h as m,c as _,x as d,b as $,y a
         Error 404: `+s[0]+`\r
     `,t=new y({props:{status:"404",message:s[0]}}),{c(){r=p(),l(t.$$.fragment)},l(e){f("svelte-aqnug2",document.head).forEach(m),r=_(e),d(t.$$.fragment,e)},m(e,o){$(e,r,o),g(t,e,o),n=!0},p(e,[o]){(!n||o&1)&&a!==(a=`\r
         Error 404: `+e[0]+`\r
-    `)&&(document.title=a)},i(e){n||(h(t.$$.fragment,e),n=!0)},o(e){E(t.$$.fragment,e),n=!1},d(e){e&&m(r),b(t,e)}}}function x(s,a,r){let t;return v(s,q,e=>r(1,t=e)),[`Not found: ${t.url.pathname??""}`]}class J extends i{constructor(a){super(),c(this,a,x,w,u,{})}}export{J as default};
+    `)&&(document.title=a)},i(e){n||(h(t.$$.fragment,e),n=!0)},o(e){E(t.$$.fragment,e),n=!1},d(e){e&&m(r),b(t,e)}}}function x(s,a,r){let t;return v(s,q,e=>r(1,t=e)),[`Not Found: ${t.url.pathname??""}`]}class F extends i{constructor(a){super(),c(this,a,x,w,u,{})}}export{F as default};
