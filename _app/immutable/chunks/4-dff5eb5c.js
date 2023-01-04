7b815adce7073af53536d1fdfb8e851427d2eac0
Teg <105562627+TegFox@users.noreply.github.com>
2023-01-03 19:34:40 -0800

    Updates

--- _app/immutable/chunks/4-dff5eb5c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/(main)/about/_page.svelte-f3974737.js";export{t as component};
---
+++ _app/immutable/chunks/4-f19ad090.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(main)/about/_page.svelte-1845107e.js";export{t as component};
